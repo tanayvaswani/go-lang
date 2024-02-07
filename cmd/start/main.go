18f8bdbdacf449753d803081f0338146ab72ff6d
tanayvaswani <vaswani.tanay9@gmail.com>
2024-02-08 01:46:30 +0530

    chore: data-types revision

--- cmd/start/main.go
+++ cmd/start/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode/utf8"
+)
 
 func main() {
 	fmt.Println("Hello, world!")
+	var a int = 10
+	fmt.Println(a)
+
+	var something bool = true
+	fmt.Println(something)
+
+	var name string = "Tanay"
+	fmt.Println(utf8.RuneCountInString(name))
+
+	// for int default = 0
+	// for strings = ""
+	// for bool = false
+
+	// Infer types
+	var fullName = "Tanay Vaswani"
+	fmt.Println(fullName)
+
+	// Short hand dropping var
+	myName := "Tanay Vaswani"
+	fmt.Println(myName)
+
+	// someFunc := foo()
+	// type unknown until hover
+	// define yourself
+
+	const pi float32 = 3.14
+	fmt.Println(pi)
 }
